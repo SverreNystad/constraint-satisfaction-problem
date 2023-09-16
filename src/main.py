1fcdb0ab0cf3e38bee839ef19d04c77a95fba3f6
Sverre Nystad <sverrenystad@gmail.com>
2023-09-16 22:54:26 +0200

    Feat: Add csp

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
 """ The main entrypoint for the application. """
 
+from Assignment import create_sudoku_csp
+
+
 def hello_world() -> str:
     """ Returns a friendly greeting. """
     return "Hello World!"
+
+
+
+EASY = "data/easy.txt"
+MEDIUM = "data/medium.txt"
+HARD = "data/hard.txt"
+VERY_HARD = "data/veryhard.txt"
+
+easy_sudoku = create_sudoku_csp(EASY)
+
+medium_sudoku = create_sudoku_csp(MEDIUM)
+hard_sudoku = create_sudoku_csp(HARD)
+very_hard_sudoku = create_sudoku_csp(VERY_HARD)
