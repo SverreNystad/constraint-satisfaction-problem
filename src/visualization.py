541ab17d3f5c49b4302b0ea8eba2041e6114d544
Sverre Nystad <sverrenystad@gmail.com>
2023-10-05 00:54:53 +0200

    Fix bug in generating sudoku image
    - Use the variable name `canvas` instead of `d` for drawing on the image.
    - Change the font size to 30 and use Arial font for drawing the numbers.
    - Set the color of the numbers to red.
    
    Co-authored-by: Simon Sandvik Lee <simonsandviklee@gmail.com>

--- src/visualization.py
+++ src/visualization.py
@@ -4,16 +4,21 @@ from PIL import Image, ImageDraw, ImageFont
 def generate_sudoku_image(solution, title: str, output_path="docs/images/"):
     # Create a new image with white background
     img = Image.new('RGB', (450, 450), color = (255, 255, 255))
-    d = ImageDraw.Draw(img)
+    canvas = ImageDraw.Draw(img)
     
     # Draw grid
     for i in range(10):
         line_width = 3 if i % 3 == 0 else 1
-        d.line([(i*50, 0), (i*50, 450)], fill=(0,0,0), width=line_width)
-        d.line([(0, i*50), (450, i*50)], fill=(0,0,0), width=line_width)
+        canvas.line([(i*50, 0), (i*50, 450)], fill=(0,0,0), width=line_width)
+        canvas.line([(0, i*50), (450, i*50)], fill=(0,0,0), width=line_width)
     
     # Load a font in the current directory
     fnt = ImageFont.load_default()
+    size = 30
+    
+    fnt = ImageFont.truetype("arial", size)
+    # Set color to red
+    color = (255, 0, 0)
     
     # Fill grid with numbers
     for row in range(9):
@@ -21,8 +26,9 @@ def generate_sudoku_image(solution, title: str, output_path="docs/images/"):
             val = solution['%d-%d' % (row, col)]
             if len(val) == 1:
                 w, h = (len(val) * 15, 30)  # Approximating width and using a standard height
-
-                d.text(((col*50)+(25-w/2), (row*50)+(25-h/2)), val[0], font=fnt, fill=(0, 0, 0))
+                number_pos = ((col*50)+(25-w/2), (row*50)+(25-h/2))
+                number = val[0]
+                canvas.text(number_pos, number, font=fnt, fill=color)
     
     # Save the image
     img.save(output_path + title + ".png")
